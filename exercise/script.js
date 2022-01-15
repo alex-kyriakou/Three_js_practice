3ae18f3153de91e8cf5eace006e6cdad5fe1b271
Alexandros <alexandr.kyriakou@gmail.com>
2022-01-15 14:21:53 +0200

    My first cube with three.js

--- exercise/script.js
+++ exercise/script.js
@@ -1,1 +1,38 @@
-console.log("hello thres.js");
+// console.log(THREE);
+// Scene
+const scene = new THREE.Scene();
+
+// Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Render Size
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.z = 4;
+camera.position.x = 0;
+camera.position.y = 1;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+// Ajsust Renderer Size, that means we resize the Canvas
+renderer.setSize(sizes.width, sizes.height);
+
+// To actually Render the scene
+renderer.render(scene, camera);
